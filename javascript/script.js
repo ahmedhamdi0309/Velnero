f5c558e2a63f78d6b859d8661025a85ed1b97c96
ahmedhamdi0309 <ahm3d.hamdy2006@gmail.com>
2026-04-22 22:39:23 +0200

    Refactor addToCartEffect function and count variable

--- javascript/script.js
+++ javascript/script.js
@@ -18,13 +18,3 @@ function addToCartEffect() {
     }
 }
 
-
-function orderNowEffect() {
-   
-    var msg = document.getElementById("order-msg");
-    msg.className = "order-notification show"; 
-    
-    
-    setTimeout(function(){ 
-        msg.className = msg.className.replace("show", ""); 
-    },4000);
